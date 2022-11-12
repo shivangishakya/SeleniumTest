79c12f28c1d5a44ff7c4fb5c1ec60a97a6f4e177
Shivangi Shakya <shivangi@Shivangis-MBP.lan>
2022-11-11 22:28:28 -0800

    Adding back fullstack-tutorial

---
+++ fullstack-tutorial/final/server/node_modules/@apollo/rover/binary.js
@@ -0,0 +1,103 @@
+const { Binary } = require("binary-install");
+const os = require("os");
+const cTable = require("console.table");
+const libc = require("detect-libc");
+
+const error = (msg) => {
+  console.error(msg);
+  process.exit(1);
+};
+
+const { version } = require("./package.json");
+const repository = "https://github.com/apollographql/rover";
+const name = "rover";
+
+const supportedPlatforms = [
+  {
+    TYPE: "Windows_NT",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-pc-windows-msvc",
+    BINARY_NAME: `${name}.exe`,
+  },
+  {
+    TYPE: "Linux",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-unknown-linux-gnu",
+    BINARY_NAME: name,
+  },
+  {
+    TYPE: "Darwin",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-apple-darwin",
+    BINARY_NAME: name,
+  },
+  {
+    TYPE: "Darwin",
+    ARCHITECTURE: "arm64",
+    RUST_TARGET: "x86_64-apple-darwin",
+    BINARY_NAME: name,
+  },
+];
+
+const getPlatform = () => {
+  const type = os.type();
+  const architecture = os.arch();
+
+  for (supportedPlatform of supportedPlatforms) {
+    if (
+      type === supportedPlatform.TYPE &&
+      architecture === supportedPlatform.ARCHITECTURE
+    ) {
+      if (supportedPlatform.TYPE === "Linux") {
+        let musl_warning = "Downloading musl binary that does not include `rover supergraph compose`.";
+        if (libc.isNonGlibcLinux) {
+          console.warn("This operating system does not support dynamic linking to glibc.");
+          console.warn(musl_warning);
+          supportedPlatform.RUST_TARGET = "x86_64-unknown-linux-musl";
+        } else {
+          let libc_version = libc.version;
+          let split_libc_version = libc_version.split(".");
+          let libc_major_version = split_libc_version[0];
+          let libc_minor_version = split_libc_version[1];
+          let min_major_version = 2;
+          let min_minor_version = 18;
+          if (libc_major_version < min_major_version || libc_minor_version < min_minor_version) {
+            console.warn(`This operating system needs glibc >= ${min_major_version}.${min_minor_version}, but only has ${libc_version} installed.`);
+            console.warn(musl_warning);
+            supportedPlatform.RUST_TARGET = "x86_64-unknown-linux-musl";
+          }
+        }
+      }
+      return supportedPlatform;
+    }
+  }
+
+  error(
+    `Platform with type "${type}" and architecture "${architecture}" is not supported by ${name}.\nYour system must be one of the following:\n\n${cTable.getTable(
+      supportedPlatforms
+    )}`
+  );
+};
+
+const getBinary = () => {
+  const platform = getPlatform();
+  // the url for this binary is constructed from values in `package.json`
+  // https://github.com/apollographql/rover/releases/download/v1.0.0/binary-install-example-v1.0.0-x86_64-apple-darwin.tar.gz
+  const url = `${repository}/releases/download/v${version}/${name}-v${version}-${platform.RUST_TARGET}.tar.gz`;
+  return new Binary(platform.BINARY_NAME, url);
+};
+
+const run = () => {
+  const binary = getBinary();
+  binary.run();
+};
+
+const install = () => {
+  const binary = getBinary();
+  binary.install();
+};
+
+module.exports = {
+  install,
+  run,
+};
